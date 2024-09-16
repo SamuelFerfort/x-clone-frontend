de507ff49905dc21d9bde268ae8a60d0a408c600
Samuel <samuferfort@gmail.com>
2024-09-16 17:33:41 +0200

    add tailwind config

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <h1>Hello, world</h1>
+    <h1 className="text-green-300">Hello, world</h1>
   )
 }
 
