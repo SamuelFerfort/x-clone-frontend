577aa95e4dd8f2ae146cc8534622b33ab78b22a6
Samuel <samuferfort@gmail.com>
2024-09-19 20:57:39 +0200

    fix post width

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -3,7 +3,7 @@ import AvatarIcon from "./Avatar";
 export default function Post({ post }) {
   return (
     <article
-      className="flex items-center p-5 border-y border-white/20 gap-2"
+      className="flex p-5 border-y border-white/20 gap-2 "
       key={post.id}
     >
       <div>
@@ -16,13 +16,15 @@ export default function Post({ post }) {
           <AvatarIcon />
         )}
       </div>
-      <div className="flex flex-col">
+      <div className="flex flex-col flex-grow min-w-0">
         <span className="flex gap-1">
-          <span className="font-bold">{post.author.username}</span>
-          <span className="text-gray-500">{post.author.handler}</span>
+          <span className="font-bold truncate">{post.author.username}</span>
+          <span className="text-gray-500 truncate">{post.author.handler}</span>
         </span>
-        <span>{post.content}</span>
+        <p className="break-words ">{post.content}</p>
+
       </div>
+
     </article>
   );
 }
