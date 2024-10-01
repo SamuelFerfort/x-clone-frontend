adf7c60605ba083ed530ff547f53a3201b18ebad
Samuel <samuferfort@gmail.com>
2024-10-01 06:49:14 +0200

    add footer to right side bar

--- src/Components/RightSideBar.jsx
+++ src/Components/RightSideBar.jsx
@@ -14,10 +14,17 @@ export default function RightSidebar() {
   }
 
   return (
-    <aside className="w-[1200px]   bg-black border-l border-white/20 fixed left-[1200px] z-20 h-screen text-white" >
+    <aside className="w-[1200px]   bg-black border-l border-white/20 fixed left-[1200px] z-20 h-screen text-white">
       <SearchBar users={data} />
 
       <WhoToFollowList data={data} isLoading={isLoading} />
+      <footer className="text-[13.5px]  text-gray-secondary text-wrap ml-9 text-center max-w-80 space-x-2 mt-1 ">
+        <a href="https://github.com/SamuelFerfort" className="hover:underline">Terms of service</a>
+        <a href="https://github.com/SamuelFerfort" className="hover:underline">Privacy Policy</a>
+        <a href="https://github.com/SamuelFerfort" className="hover:underline  ">Cookie Policy</a>
+        <a href="https://github.com/SamuelFerfort" className="hover:underline">Accessibility</a>
+        <a href="https://github.com/SamuelFerfort" className="hover:underline">Ads Info</a>
+      </footer>
     </aside>
   );
 }
