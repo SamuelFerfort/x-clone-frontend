fb8e4b6db6ec58fb3a8851da6a72a73d37b92fab
Samuel <samuferfort@gmail.com>
2024-10-01 06:08:26 +0200

    set autocomplete off

--- src/Components/StyledFormInput.jsx
+++ src/Components/StyledFormInput.jsx
@@ -1,38 +1,50 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-const StyledFormInput = ({ label, value, onChange, maxLength, isTextArea = false }) => {
+const StyledFormInput = ({
+  label,
+  value,
+  onChange,
+  maxLength,
+  isTextArea = false,
+}) => {
   const [isFocused, setIsFocused] = useState(false);
 
-  const InputComponent = isTextArea ? 'textarea' : 'input';
+  const InputComponent = isTextArea ? "textarea" : "input";
 
   return (
-    <div 
+    <div
       className={`border rounded-md px-3 py-1 flex flex-col transition-colors duration-200 ${
-        isFocused ? 'border-btn-blue' : 'border-white/20'
-      } ${isTextArea ? 'h-20 mt-8' : 'h-14 mt-20'}`}
+        isFocused ? "border-btn-blue" : "border-white/20"
+      } ${isTextArea ? "h-20 mt-8" : "h-14 mt-20"}`}
     >
       <div className="flex justify-between">
-        <label 
-          htmlFor={label.toLowerCase()} 
+        <label
+          htmlFor={label.toLowerCase()}
           className={`transition-colors duration-200 ${
-            isFocused ? 'text-btn-blue' : 'text-white/40'
+            isFocused ? "text-btn-blue" : "text-white/40"
           }`}
         >
           {label}
         </label>
-        <span className="text-white/40 text-sm">{value.length}/{maxLength}</span>
+        <span className="text-white/40 text-sm">
+          {value.length}/{maxLength}
+        </span>
       </div>
       <InputComponent
         type="text"
         id={label.toLowerCase()}
         value={value}
         onChange={(e) => {
-          if (e.target.value.length <= maxLength || e.target.value.length < value.length) {
+          if (
+            e.target.value.length <= maxLength ||
+            e.target.value.length < value.length
+          ) {
             onChange(e.target.value);
           }
         }}
         onFocus={() => setIsFocused(true)}
         onBlur={() => setIsFocused(false)}
+        autoComplete="off"
         name={label.toLowerCase()}
         placeholder={isTextArea ? "I like turtles..." : ""}
         className="bg-black outline-none w-full text-white resize-none placeholder:text-white/40"
@@ -41,4 +53,4 @@ const StyledFormInput = ({ label, value, onChange, maxLength, isTextArea = false
   );
 };
 
-export default StyledFormInput
+export default StyledFormInput;
